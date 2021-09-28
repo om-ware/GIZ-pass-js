b41c0509d95da580cf7e72bd2f13f5f64d847704
om-ware <omar.cs536@student.uomosul.edu.iq>
2021-09-28 12:27:21 -0700

    update if statement

--- task.js
+++ task.js
@@ -15,7 +15,7 @@ for(let i = 0 ; i < numbers.length ; i++)
 {
     for(let j = 0 ; j < numbers.length - 1 ; j++)
     {
-        if(numbers[i] > numbers[j + 1])
+        if(numbers[j] > numbers[j + 1])
         {
             let temp = numbers[j];
             numbers[j] = numbers[j + 1]
@@ -30,7 +30,7 @@ for(let i = 0 ; i < numbers.length ; i++)
 {
     for(let j = 0 ; j < numbers.length - 1 ; j++)
     {
-        if(numbers[i] < numbers[j + 1])
+        if(numbers[j] < numbers[j + 1])
         {
             let temp = numbers[j];
             numbers[j] = numbers[j + 1]
